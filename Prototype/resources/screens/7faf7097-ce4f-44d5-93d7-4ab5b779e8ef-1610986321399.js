72066a9fc6d8b44abcba7d116061f6d28efe98a6
kunjd96 <kunjd96@gmail.com>
2021-01-18 11:13:56 -0500

    Prototpe
    Login Splashscreen Signup design

---
+++ Prototype/resources/screens/7faf7097-ce4f-44d5-93d7-4ab5b779e8ef-1610986321399.js
@@ -0,0 +1,35 @@
+jQuery("#simulation")
+  .on("click", ".s-7faf7097-ce4f-44d5-93d7-4ab5b779e8ef .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Paragraph_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/13bf2996-533f-47db-ae9c-9f79f6056fe4",
+                    "transition": {
+                      "type": "slideleft",
+                      "duration": 900
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
---
+++ Prototype/resources/screens/9d954f5b-159d-4fcc-a700-cc3eb4b85ff5-1610986321399.js
@@ -0,0 +1,35 @@
+jQuery("#simulation")
+  .on("click", ".s-9d954f5b-159d-4fcc-a700-cc3eb4b85ff5 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Button_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/9ebe2c67-939b-42aa-87a6-791fe0cac427",
+                    "transition": {
+                      "type": "slideleft",
+                      "duration": 900
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
