ff5bb987f5abf9a053905f8843056c83e58df9e5
kunjd96 <kunjd96@gmail.com>
2021-01-27 19:22:55 -0500

    nodejs 2

--- Nodejs/config/db.js
+++ Nodejs/config/db.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
-const keys = require ('./keys');
 
-const connectDB = async () => {
-    const conn = await mongoose.connect(keys.MONGO_URI,{
-            useNewUserParser : true,
-            useCreateIndex : true,
-            useFindAnyModify: false,
-            useUnifiedTopology : true,
-            useNewUrlParser: true
+
+const connectDB = async() => {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
     });
-    console.log("MongoDB Connected",conn.connection.host);
+    console.log("MongoDB Connected", conn.connection.host);
 };
 
 module.exports = connectDB;
